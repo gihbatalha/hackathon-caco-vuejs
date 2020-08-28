1ed85631c4a4ce5e1b2055e2422f2f5f3097dee6
Giovanna Batalha <giovanna@Giovanna.local>
2020-08-28 17:31:32 -0300

    :tada: Estrutura do projeto com vue-cli

---
+++ app/src/views/First.vue
@@ -0,0 +1,46 @@
+<template>
+    <span> Hello World </span>
+</template>
+
+<style scoped>
+    span{
+        color: red;
+    }
+</style>
+
+<script>
+    // Básico
+    // 1. print variável {message}
+    // 2. v-model 
+    // 3. atributes :bind :color=""
+    // 3. v-if/v-else
+    // 4. v-for
+    // 5. ouvir eventos de componentes basicos do html @input @click
+
+
+    //Comunicação entre componentes
+    // --- comunicação pai-filho
+    // passar props => estático ou dinâmico (:)
+    // watch das props primitivas
+    // props objeto (vai sozinha?)
+    //--- comunicação filho-pai
+    // eventos do componente filho
+
+    //Extra - fazer ao fim somente
+    //slots
+
+    //Exemplo master
+    //três camadas de componente para deixar a página dark
+    // página - header - seletor (dark/ou nao)
+
+    //Estrutura do projeto - mostrar main.js + index.html, App.vue, views e components
+
+    //Vue Router
+    //Mostrar no projeto
+
+    //Vuetify
+    // Fazer um exemplo simples usando algum componente do vuetify
+    // entrar no site
+</script>
+
+
